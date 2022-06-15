f86fb207f814db16c2b08398aea0614138621539
Derek Grove <derekjgrove@gmail.com>
2022-06-15 13:41:16 -0400

    Added MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import styles from './App.css';
 import Logo from './assets/icons/logo.svg'
-import Gohan from './assets/img/gohan.jpg'
-
-// const App = ({ title }) => <div className={styles.title}>
-//     {title} 
-//     <Logo width="50px" height="50px"/>
-//     <img src={Gohan}/>
-//     </div>;
-
 class App extends React.Component {
 
+    
     render () {
         return (
-            <div className={styles.title}>
-                {this.props.title} 
-                <Logo width="50px" height="50px"/>
-                <img src={Gohan}/>
-            </div>
+            <React.Fragment>
+                <CssBaseline />
+                <div className={styles.title}>
+                    {this.props.title} 
+                    <Logo width="50px" height="50px"/>
+                </div>
+            </React.Fragment>
         )
     }
 }
